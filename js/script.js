8f8738d0929fa0435978847adc3a5d4a6b59d2dc
reham90 <rehamadel344@gmail.com>
2023-09-20 21:38:30 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -185,7 +185,87 @@ function manageDrop(){
 
  
 
-// 
+///////////////////////////////////       validation of vendors form             /////////////////////////////////////////////
+
+var name = document.getElementById("name");
+name.onkeypress = function(e) {
+    e = e || window.event;
+    var charCode = (typeof e.which == "undefined") ? e.keyCode : e.which;
+    var charStr = String.fromCharCode(charCode);
+    if (/\d/.test(charStr)) {
+        return false;
+    }
+};
+/////////////////////////////////// custom select     ///////////////////////////////////////
+
+
+
+ ///////// ** upload images ** /////////
+
+
+
+ ImgUpload();
+
+
+ function ImgUpload() {
+     var imgWrap = "";
+     var imgArray = [];
+
+     $('.upload__inputfile').each(function() {
+         $(this).on('change', function(e) {
+             imgWrap = $(this).closest('.upload__box').find('.upload__img-wrap');
+             var maxLength = $(this).attr('data-max_length');
+
+             var files = e.target.files;
+             var filesArr = Array.prototype.slice.call(files);
+             var iterator = 0;
+             filesArr.forEach(function(f, index) {
+
+                 if (!f.type.match('image.*')) {
+                     return;
+                 }
+
+                 if (imgArray.length > maxLength) {
+                     return false
+                 } else {
+                     var len = 0;
+                     for (var i = 0; i < imgArray.length; i++) {
+                         if (imgArray[i] !== undefined) {
+                             len++;
+                         }
+                     }
+                     if (len > maxLength) {
+                         return false;
+                     } else {
+                         imgArray.push(f);
+
+                         var reader = new FileReader();
+                         reader.onload = function(e) {
+                             var html = "<div class='upload__img-box'><div style='background-image: url(" + e.target.result + ")' data-number='" + $(".upload__img-close").length + "' data-file='" + f.name + "' class='img-bg'><div class='upload__img-close'></div></div></div>";
+                             imgWrap.append(html);
+                             iterator++;
+                         }
+                         reader.readAsDataURL(f);
+                     }
+                 }
+             });
+         });
+     });
+
+     $('body').on('click', ".upload__img-close", function(e) {
+         var file = $(this).parent().data("file");
+         for (var i = 0; i < imgArray.length; i++) {
+             if (imgArray[i].name === file) {
+                 imgArray.splice(i, 1);
+                 break;
+             }
+         }
+         $(this).parent().parent().remove();
+     });
+ }
+
+
+
   //////////** fixed arrow to top**//////////
   $(".arrow-top").click(function() {
       $("html,body").animate({
