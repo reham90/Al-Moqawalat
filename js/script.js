372fe3067a80dfc926a0d4e7b527f04005a91f81
reham90 <rehamadel344@gmail.com>
2023-09-21 15:53:55 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -77,36 +77,6 @@ function manageDrop(){
   }
   
   manageDrop();
-
-////////////** counter  *///////////////////////////////
-var ax = 0;
-$(window).scroll(function() {
-
-    var oTop = $('#counter').offset().top - window.innerHeight;
-    // Md.Asaduzzaman Muhid
-    if (ax == 0 && $(window).scrollTop() > oTop) {
-        $('.counter-number').each(function() {
-            var $this = $(this);
-            jQuery({
-                Counter: 0
-            }).animate({
-                Counter: $this.text()
-            }, {
-                duration: 2000,
-                easing: 'swing',
-                step: function() {
-                    $this.text(Math.ceil(this.Counter));
-                }
-            });
-        });
-        ax = 1; // Md.Asaduzzaman Muhid
-    }
-});
-
-
-
-
-
   //fixed nav
   $stickyNav = $(".top-header");
   $(window).scroll("scroll load", function() {
@@ -146,6 +116,37 @@ $(window).scroll(function() {
   });
 
  
+
+////////////** counter  *///////////////////////////////
+var ax = 0;
+$(window).scroll(function() {
+
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    // Md.Asaduzzaman Muhid
+    if (ax == 0 && $(window).scrollTop() > oTop) {
+        $('.counter-number').each(function() {
+            var $this = $(this);
+            jQuery({
+                Counter: 0
+            }).animate({
+                Counter: $this.text()
+            }, {
+                duration: 2000,
+                easing: 'swing',
+                step: function() {
+                    $this.text(Math.ceil(this.Counter));
+                }
+            });
+        });
+        ax = 1; // Md.Asaduzzaman Muhid
+    }
+});
+
+
+
+
+
+  
 
   /// ////// ** category-slider-for-mobile-view** /////////
   if ($(window).width() <= 991) {
