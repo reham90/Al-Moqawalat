4f503214958de044734314be7b42a13bed9383ba
reham90 <rehamadel344@gmail.com>
2023-10-31 17:42:06 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -7,24 +7,42 @@ $(document).ready(function() {
 
   //phone size menu onclick
   if ($(window).width() <= 991) {
-      $(".menu-id").click(function(e) {
-          e.preventDefault();
-          $('.menu-bars .bar').toggleClass('hide-icon');
-          $('.menu-bars .times').toggleClass('hide-icon');
-          $(".navgition").toggleClass("reset-left");
-          $("body").toggleClass("overflow");
+    $("#menu-id").click(function(e) {
+        e.preventDefault();
+        $('.menu-bars .bar').toggleClass('hide-icon');
+        $('.menu-bars .times').toggleClass('hide-icon');
+        $(".navgition").toggleClass("reset-left");
+        $("body").toggleClass("overflow");
 
-      });
-      $(".nav-head .close-btn").click(function() {
-          $(".navgition").removeClass("reset-left");
-          $(".menu-bars .bars").toggleClass("open-bars");
-          $(".menu-bars .bars").toggleClass("close-bars");
-         
-      });
-
-
+    });
+    $(".nav-head .close-btn").click(function() {
+        $(".navgition").removeClass("reset-left");
+        $(".menu-bars .bars").toggleClass("open-bars");
+        $(".menu-bars .bars").toggleClass("close-bars");
+        $("body").removeClass("overflow");
+    });
 
 
+
+   //slide down menu
+   $(".menu-name").click(function(e) {
+    e.preventDefault();
+    $(this).siblings(".cats-display-2").slideToggle(400);
+    $(".menu-name").not(this).siblings(".cats-display-2").slideUp(400);
+    if ($(window).width() <= 991) {
+        $(this).toggleClass("active");
+        $(".menu-name").not(this).removeClass("active");
+    }
+});
+$(".slide-menu-2").click(function(e) {
+    e.preventDefault();
+    $(this).siblings(".cats-dispaly-3").slideToggle(400);
+    $(".slide-menu-2").not(this).siblings(".cats-dispaly-3").slideUp(400);
+    if ($(window).width() <= 991) {
+        $(this).toggleClass("active");
+        $(".slide-menu-2").not(this).removeClass("active");
+    }
+});
   
 
   };
